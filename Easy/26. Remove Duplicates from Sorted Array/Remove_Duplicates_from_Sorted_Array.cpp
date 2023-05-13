7f551120a7be63e3169c098ab89ffd6ad1b3dc21
AshenOne17 <artem08.mail.com@gmail.com>
2023-05-13 16:35:17 +0200

    Added alternative solution.

--- Easy/26. Remove Duplicates from Sorted Array/Remove_Duplicates_from_Sorted_Array.cpp
+++ Easy/26. Remove Duplicates from Sorted Array/Remove_Duplicates_from_Sorted_Array.cpp
@@ -106,6 +106,32 @@ public:
 };
 */
 
+// Alternative easy solution with two-pointers approach
+
+/*
+class Solution {
+public:
+
+    int removeDuplicates(std::vector<int>& nums) {
+        
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int slow = 0;
+
+        for (int fast = 1; fast < nums.size(); fast++){
+            if (nums[slow] != nums[fast]){
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+
+        return slow + 1;
+    }
+};
+*/
+
 // Code for testing
 // Should produce result 5 
 
