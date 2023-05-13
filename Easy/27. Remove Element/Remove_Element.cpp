e8e66ee25daa89a6d0317939a9495572c0016136
AshenOne17 <artem08.mail.com@gmail.com>
2023-05-13 16:32:00 +0200

    Revert "Added alternative solution."
    This reverts commit 5c4e063ae16e90094a687dd7c902356d0c6d38ef.

--- Easy/27. Remove Element/Remove_Element.cpp
+++ Easy/27. Remove Element/Remove_Element.cpp
@@ -89,32 +89,6 @@ public:
     }
 };
 
-// Alterantive two-pointers simple solution
-
-/*
-class Solution {
-public:
-
-    int removeDuplicates(std::vector<int>& nums) {
-        
-        if (nums.empty()) {
-            return 0;
-        }
-
-        int slow = 0;
-
-        for (int fast = 1; fast < nums.size(); fast++){
-            if (nums[slow] != nums[fast]){
-                slow++;
-                nums[slow] = nums[fast];
-            }
-        }
-
-        return slow + 1;
-    }
-};
-*/
-
 // Code for testing
 // Should produce result 2
 
